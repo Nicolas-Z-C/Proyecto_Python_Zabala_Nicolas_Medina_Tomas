7611315d48df932d08fd6a37f635b53e34f9078f
Nicolas-Z-C <nicolascampus117@gmail.com>
2025-08-22 07:13:23 -0500

    Implemented the main menu to the main file

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
+import modules.corefiles as core
+import modules.menufiles as menus
 if __name__ == "__main__":
-    pass
+ print("""
+       -------------------------------------------------------------
+       |***Bienvenido al sistema de gestion de campers y trainers***
+       -------------------------------------------------------------
+       """)
+ core.Wait(1)
+
+while True:
+ core.clr()
+
+ op = menus.MainMenu()
+ match op:
+  case "1":
+    core.CrearCuenta()
+  case "2":
+    core.CambiarEstatusCamper()
+  case "3":
+    core.CambiarNotas()
+  case "4":
+    core.CrearNuevaRuta()
+  case "5":
+    core.MatricularCampers()
+  case "6":
+    core.Reportes()
+  case "7":
+    core.BorrarCuenta()
+  case "8":
+    print("Saliendo del sistema...")
+    break
+  case _:
+    print("Opcion no valida, por favor intente de nuevo.")
+  
