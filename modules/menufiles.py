f8d4a01c37d5517aea255d13f5173e27c809b0a5
Nicolas-Z-C <nicolascampus117@gmail.com>
2025-08-22 06:58:08 -0500

    Made the main menu

---
+++ modules/menufiles.py
@@ -0,0 +1,20 @@
+
+import modules.corefiles as core 
+
+def MainMenu():
+    core.clr()
+    print("""
+        **** Menu Principal ****       
+    --------------------------------------------
+        1. Crear Cuenta (Camper/Trainer)
+        2. Cambiar Estatus Camper
+        3. Cambiar Notas
+        4. Crear Nueva Ruta
+        5. Matricular Campers
+        6. Reportes
+        7. Borrar Cuenta(Camper/Trainer)
+        8. Salir   
+    --------------------------------------------
+          
+    Porfavor elija una opcion:
+    """)
