6d791b0539caf1202c0e634baea58a8ce33e15e9
Nicolas-Z-C <nicolascampus117@gmail.com>
2025-08-22 06:46:22 -0500

    Started the main functions on the corefiles

---
+++ modules/corefiles.py
@@ -0,0 +1,70 @@
+import os
+import time
+def clr(): 
+ os.system('cls' if os.name == 'nt' else 'clear') 
+
+def Wait(S:int):
+ time.sleep(S)
+
+def CrearCuenta():
+ pass
+
+def Login():
+ pass
+
+def Logout():
+ pass
+
+def BorrarCuenta():
+ pass
+
+def CambiarContraseña():
+ pass
+
+def CambiarNota():
+ pass
+
+def VerNotas():
+ pass
+
+def VerUsuarios():
+ pass   
+
+def CambiarEstatus():
+ pass   
+
+def NuevaRuta():
+ pass
+
+def VerRutas():
+ pass
+
+def BorrarRuta():
+ pass 
+
+def AgregarARuta():
+ pass
+
+def EliminarDeRuta():
+ pass
+
+def CampersInscritos():
+ pass
+
+def CampersAprobados():
+ pass
+
+def Trainers():
+ pass
+
+def CampersBajoRendimiento():
+ pass  
+
+def CampersYTrainersMismaRuta():
+ pass
+
+def Estadisticas():
+ pass
+
+
+
